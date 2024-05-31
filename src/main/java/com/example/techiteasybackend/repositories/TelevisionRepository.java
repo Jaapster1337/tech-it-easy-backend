8d5e5170507b76fd83bb6676f7d9f909a4f0b7c0
LorelessVictory <ejboutens1337@gmail.com>
2024-05-31 11:25:18 +0200

    huiswerk2 af

---
+++ src/main/java/com/example/techiteasybackend/repositories/TelevisionRepository.java
@@ -0,0 +1,8 @@
+package com.example.techiteasybackend.repositories;
+
+import com.example.techiteasybackend.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
+
+}
